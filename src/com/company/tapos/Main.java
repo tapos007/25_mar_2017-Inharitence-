554b062763d33693aa06006b2e53a325cfac7f45
tapos <tapos.aa@gmail.com>
2017-03-25 11:59:49 +0600

    initial commit done

---
+++ src/com/company/tapos/Main.java
@@ -0,0 +1,14 @@
+package com.company.tapos;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // encapsulation
+        // inharitence
+        // abstraction
+        // polymorphism
+
+    }
+}
